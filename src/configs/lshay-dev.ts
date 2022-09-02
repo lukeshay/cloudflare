2604f553a51495497265fcead9ac9b07a5a3a67e
Luke Shay <shay.luke17@gmail.com>
2022-09-02 14:16:57 -0500

    Mail stuff

--- src/configs/lshay-dev.ts
+++ src/configs/lshay-dev.ts
@@ -1,6 +1,6 @@
-import { Zone } from "@pulumi/cloudflare";
+import { Record, Zone } from "@pulumi/cloudflare";
 
-new Zone(
+const zone = new Zone(
 	"lshay-dev",
 	{
 		plan: "free",
@@ -10,3 +10,29 @@ new Zone(
 		protect: true,
 	},
 );
+
+new Record("mail-server-1", {
+	name: "@",
+	priority: 10,
+	proxied: false,
+	type: "MX",
+	value: "mx1.privateemail.com",
+	zoneId: zone.id,
+});
+
+new Record("mail-server-2", {
+	name: "@",
+	priority: 10,
+	proxied: false,
+	type: "MX",
+	value: "mx2.privateemail.com",
+	zoneId: zone.id,
+});
+
+new Record("mail-server-txt", {
+	name: "@",
+	proxied: false,
+	type: "TXT",
+	value: "v=spf1 include:spf.privateemail.com ~all",
+	zoneId: zone.id,
+});
